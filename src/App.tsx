f2542639f4eb67b4911f7f9c265c76dec5c53cc3
Rafaela Lucia <rafaluciadev@gmail.com>
2023-04-11 19:08:12 -0300

    inicio de material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import { useState } from 'react'
+import { Grid, Paper } from '@material-ui/core'
+import './App.css'
 import Home from './pages/home/Home'
+import Navbar from './components/static/navbar/Navbar'
+import Footer from './components/static/footer/Footer'
 
 function App() {
   return (
-   <Home/> 
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
   )
 }
 
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Box, Paper, Button, Grid, Typography } from '@material-ui/core';
 import './Home.css';
 
 import Photo from '../../assets/HomePhoto.png'
@@ -9,20 +10,26 @@ let mensagem = '';
 
 function Home() {
 
-    if(hora >= 6 && hora < 12){
-        mensagem = 'Bom dia!'
-    } else if(hora >= 12 && hora < 19){
-        mensagem = 'Boa tarde!'
-    } else if(hora >= 19 && hora <= 0o0){
-        mensagem = 'Boa noite!'
-    }
-
-    return(
+    return (
         <>
-        <h3 className='titulo'>{mensagem}</h3>
-        <h1 className="titulo">Bem vindo(a)</h1>
-        <p className='paragraph'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis quam quod laudantium, repudiandae ex aperiam officiis nihil, est ab amet deserunt veritatis sit debitis blanditiis beatae temporibus saepe quos officia.</p>
-        <img src={Photo} alt="Imagem Tela Inicial" className = 'img'/>
+              <Grid container direction="row" justifyContent="center" alignItems="center">
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "black", fontWeight: "bold" }}>Seja bem vindo(a)!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "black", fontWeight: "bold" }}>expresse aqui os seus pensamentos e opiniões!</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                        </Box>
+                        <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#794DB7", color: "white" }}>Ver Postagens</Button>
+                    </Box>
+                </Grid>
+                <Grid item xs={6} >
+                    <img src="https://images.unsplash.com/photo-1493612276216-ee3925520721?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80"alt="" width="530px" height="500px" />
+                </Grid>
+                <Grid xs={12} style={{ backgroundColor: "white" }}>
+                </Grid>
+            </Grid>
         </>
     );
 }
